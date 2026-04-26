45be2c42223285754b1c4c8436c5989cc761a60b
2410020049miski <2410020049miski@gmail.com>
2026-04-26 20:37:05 +0800

    Add files via upload

---
+++ tugas2/src/main/java/com/mycompany/tugas2/Main.java
@@ -0,0 +1,24 @@
+package com.mycompany.tugas2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Object dari constructor tanpa parameter
+        Skincare sc1 = new Skincare();
+        System.out.println("Data Skincare 1:");
+        sc1.tampilkanData();
+
+        System.out.println();
+
+        // Object dari constructor dengan parameter
+        Skincare sc2 = new Skincare("Facial Wash", "Kulit Berminyak", 50000);
+        System.out.println("Data Skincare 2:");
+        sc2.tampilkanData();
+
+        System.out.println();
+
+        // Method dengan nilai balik
+        System.out.println("Info Produk:");
+        System.out.println(sc2.getInfoProduk());
+    }
+}
